b8512bf9b8b297d7a693b1571a03cac9a0ce854a
faSUro <fasulo.nicol@gmail.com>
2020-07-22 21:58:52 +0200

    added e2d.py (lib) and client.py

---
+++ client.py
@@ -0,0 +1,48 @@
+import argparse
+import csv
+import socket
+import e2d
+
+parser = argparse.ArgumentParser(description="Client per l'avvio di una simulazione Energy2D  e l'acquisizione dei "
+                                             "dati generati")
+parser.add_argument("steps", type=int, help="Quantità di step da simulare")
+parser.add_argument("steplength", type=int, help="Durata di uno step nella simulazione in secondi")
+args = parser.parse_args()
+
+
+thermostats = e2d.initialize_thermostats()  # inizializza il dizionario dei termostati
+columns = e2d.initialize_data_columns(thermostats)  # inizializza l'array di colonne del csv con i dati
+
+serverAddressPort = e2d.get_server_address_and_port()  # coppia indirizzo-porta del server
+UDPClientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)  # crea una socket UDP
+
+csv_file = open(e2d.simulationDataFile, mode='w')  # apre il file csv per salvare i dati
+writer = csv.DictWriter(csv_file, fieldnames=columns)
+writer.writeheader()
+
+e2d.send_message(UDPClientSocket, serverAddressPort, e2d.set_steplength_command(args.steplength))  # setta steplength
+e2d.send_message(UDPClientSocket, serverAddressPort, e2d.reset_command())  # resetta la simulazione
+e2d.send_message(UDPClientSocket, serverAddressPort, e2d.run_command(1))  # esegue la simulazione per un secondo,
+# tempo necessario affinché i termometri rilevino una temperatura valida
+step = 0
+while step < args.steps:  # ciclo principale che esegue la simulazione e raccoglie dati
+    e2d.send_message(UDPClientSocket, serverAddressPort, e2d.runsteps_command(str(args.steplength)))
+
+    fileRow = {"TIME": step}
+    answer = e2d.send_message(UDPClientSocket, serverAddressPort, e2d.get_temperatures_command())
+    temperatures = answer.split("   ")
+    for t in temperatures:
+        tSplit = t.split(" ")
+        thermometerName = tSplit[0]
+        if thermometerName.startswith(e2d.thermometerPrefix):
+            temperature = tSplit[1]
+            if thermometerName != "THERMOMETER_EXTERNAL":
+                thermostats.get(thermometerName).update(UDPClientSocket, serverAddressPort, temperature)
+
+                fileRow[thermometerName] = thermostats.get(thermometerName).temperature
+                fileRow[e2d.get_heater_name(thermometerName)] = thermostats.get(thermometerName).heaterIsOn
+            else:
+                fileRow["THERMOMETER_EXTERNAL"] = temperature
+
+    writer.writerow(fileRow)
+    step += args.steplength
---
+++ e2d.py
@@ -0,0 +1,127 @@
+import json
+import os
+
+
+thermometerPrefix = "THERMOMETER"
+heaterPrefix = "HEATER"
+
+thermostatSettingsFile = os.getcwd() + "/config/thermostat-settings.json"
+generalSettingsFile = os.getcwd() + "/config/general-settings.json"
+simulationDataFile = os.getcwd() + "/simulation_data/data.csv"
+
+
+def set_heater_power_command(heater, power):
+    return "set " + heater + ".power " + str(power)
+
+
+def set_steplength_command(steplength):
+    return "set timestep " + str(steplength)
+
+
+def reset_command():
+    return "reset"
+
+
+def run_command(n):
+    return "run " + str(n)
+
+
+def get_temperatures_command():
+    return "get allthermometers.temperature"
+
+
+def runsteps_command(n):
+    return "runsteps " + str(n)
+
+
+def send_message(client_socket, server_address_port, message):
+    req = str.encode(message)
+    client_socket.sendto(req, server_address_port)  # sends to server using created UDP socket
+
+    buffer_size = 4194304  # 4MB
+    resp = client_socket.recvfrom(buffer_size)
+    return str(resp[0], 'utf-8')  # ritorna la risposta del server decodificata
+
+
+def get_heater_name(thermometer):  # metodo che ritorna il nome del termosifone a partire dal nome del termometro
+    heater = heaterPrefix + thermometer[11:]
+    return heater
+
+
+def initialize_data_columns(thermostats):
+    columns = ["TIME", "THERMOMETER_EXTERNAL"]
+    for ts in thermostats:  # riempie la lista contenente le colonne con i nomi dei termometri e dei termosifoni
+        columns.append(thermostats.get(ts).thermometerName)
+        columns.append(thermostats.get(ts).heaterName)
+    return columns
+
+
+class Thermostat:  # classe che rappresenta il termostato
+    def __init__(self, thermometer_name, setpoint, deadband):
+        self.thermometerName = thermometer_name
+        self.temperature = 0.0
+        self.setpoint = float(setpoint)
+        self.deadband = float(deadband)
+        self.heaterName = get_heater_name(thermometer_name)
+        self.heaterIsOn = 0
+
+    def update(self, clientsocket, server_address_port, temp):
+        self.temperature = float(temp)
+        message = "none"
+        if (self.setpoint - self.temperature) > self.deadband:  # se la temperatura è troppo bassa
+            message = set_heater_power_command(self.heaterName, power=100)  # comando che accende il termosifone
+            self.heaterIsOn = 10
+        elif (self.temperature - self.setpoint) > self.deadband:  # se la temperatura è troppo alta
+            message = set_heater_power_command(self.heaterName, power=0)  # comando che spegne il termosifone
+            self.heaterIsOn = 0
+        send_message(clientsocket, server_address_port, message)
+
+
+def get_thermometers_name_from_json():
+    data = json.load(open(thermostatSettingsFile))  # apre il file di configurazione dei termostati
+    return list(data.keys())
+
+
+def initialize_thermostats():
+    data = json.load(open(thermostatSettingsFile))
+    thermometers_name = list(data.keys())
+    thermostats = {}
+    for tm in thermometers_name:
+        thermostats[tm] = Thermostat(tm, data.get(tm).get("setpoint"), data.get(tm).get("deadband"))
+    return thermostats
+
+
+def get_thermometers_name_from_server(client_socket, server_address_port):
+    thermometers = []
+    answer = send_message(client_socket, server_address_port, get_temperatures_command())
+    split_answer = answer.split("   ")
+    for t in split_answer:
+        split = t.split(" ")
+        thermometer_name = split[0]
+        if (thermometer_name.startswith(thermometerPrefix)) & (thermometer_name != "THERMOMETER_EXTERNAL"):
+            thermometers.append(thermometer_name)
+
+    return thermometers
+
+
+def build_default_thermostat_dict(thermometers):
+    thermostats = {}
+    for t in thermometers:
+        thermostats[t] = {
+            "setpoint": 20.0,
+            "deadband": 0.5
+        }
+    return thermostats
+
+
+def get_server_address_and_port():
+    settings_file = json.load(open(generalSettingsFile))
+    address_port = (settings_file.get("server_address"), settings_file.get("server_port"))
+    return address_port
+
+
+def get_empty_list_of_lists(size):
+    list_of_lists = []
+    for i in range(size):
+        list_of_lists.append([])
+    return list_of_lists
